7f87f19def2348600441511e44af6bbb7d516063
Kenzo <79611160+Kenzo-Sugai@users.noreply.github.com>
2022-08-29 17:40:12 -0300

    Add files via upload

---
+++ 1093.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  int EV1, EV2, AT, D;
+
+  while(cin >> EV1 >> EV2 >> AT >> D){
+    double resp = AT*100/6;
+    cout << resp << endl;
+     
+  }
+}
